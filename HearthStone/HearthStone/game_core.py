851bde323f2ba6e7503d4408da11a74d622eb839
fanyang <fyabc@mail.ustc.edu.cn>
2016-11-28 22:22:16 +0800

    Add more in Game class of HearthStone.

--- HearthStone/HearthStone/game_core.py
+++ HearthStone/HearthStone/game_core.py
@@ -7,8 +7,14 @@ from HearthStone.event_framework import EventEngine
 __author__ = 'fyabc'
 
 
-class Hero:
-    pass
+class HistoryManager:
+    def __init__(self, game):
+        self.game = game
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
 
 
 class Game:
@@ -21,18 +27,36 @@ class Game:
             minions
             cards
             heroes
-            (history manager)
+        history manager
             ...
     """
+
+    # Constants.
+    TotalPlayerNumber = 2
 
     def __init__(self):
         # Event engine.
         self.engine = EventEngine()
 
         # Game data.
+        self.players = [Player(self) for _ in range(self.TotalPlayerNumber)]
+        self.current_player_id = 0
 
+        # History manager.
+        self.history = HistoryManager(self)
+
+    # Properties.
+    @property
+    def current_player(self):
+        return self.players[self.current_player_id]
+
+    # Events and handlers.
     def create_event(self, event_type, *args, **kwargs):
         return event_type(self, *args, **kwargs)
 
     def create_handler(self, handler_type, *args, **kwargs):
         return handler_type(self, *args, **kwargs)
+
+    # Game operations.
+    def next_turn(self):
+        self.current_player_id = (self.current_player_id + 1) % self.TotalPlayerNumber
