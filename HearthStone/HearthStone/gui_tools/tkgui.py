b451a8546d52ab80186938219a99853f3fb7ff31
fyabc <fyabc@mail.ustc.edu.cn>
2016-12-05 02:14:33 +0800

    Add more in Tk GUI tool, to fix in the layout.

--- HearthStone/HearthStone/gui_tools/tkgui.py
+++ HearthStone/HearthStone/gui_tools/tkgui.py
@@ -1,74 +1,142 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import tkinter as tk
+from functools import wraps
+from tkinter import *
+from tkinter.ttk import *
+
+from ..game_events import GameBegin, GameEnd, TurnEnd, SummonMinion
 
 __author__ = 'fyabc'
 
 
-class GameWindow(tk.Frame):
+class GameWindow(Frame):
+    """The Tk game window.
+
+    """
+
+    # [NOTE] Some rules of the variable's name in the layout:
+    # There are two players in the game. I am Player 0, opponent is Player 1.
+    # `self.deck_frame_0`, `self.deck_frame_1`
+
+    # [NOTE] Selection states:
+    #   0 = not anything selected
+    #   n = n objects selected
+
     def __init__(self, game, master=None):
         super(GameWindow, self).__init__(master=master, borderwidth=30)
-        self.pack(fill=tk.BOTH)
+        self.pack(fill=BOTH)
 
         self.game = game
+
+        self.selection_state = 0
 
         ########
         # Menu #
         ########
 
-        self.menu_bar = tk.Menu(self.master)
+        self.menu_bar = Menu(self.master)
         self.master.config(menu=self.menu_bar)
 
         # All cascade menus.
         self.cascade_menus = {
-            'Game': tk.Menu(self.menu_bar),
-            'Help': tk.Menu(self.menu_bar),
+            'Game': Menu(self.menu_bar),
+            'Help': Menu(self.menu_bar),
         }
 
         self.menu_bar.add_cascade(label='Game', menu=self.cascade_menus['Game'])
+        self.menu_bar.add_command(label='Game Begin', command=self.game_begin)
+        self.menu_bar.add_command(label='Refresh', command=self.refresh_window)
         self.menu_bar.add_cascade(label='Help', menu=self.cascade_menus['Help'])
         self.menu_bar.add_command(label='Quit', command=self.quit)
 
-        ##################
-        # Window Layout. #
-        ##################
+        ###############################
+        # Window Layout.              #
+        #                             #
+        #   -----------------------   #
+        #   |    |   .   .   |    |   #
+        #   -----------------------   #
+        #   |    |   .   .   |    |   #
+        #   -----------------------   #
+        #                             #
+        ###############################
 
         ########################
         # Left: History frame. #
         ########################
-        self.history_frame = tk.Frame(self, borderwidth=0)
-        self.history_frame.pack(side=tk.LEFT)
+        self.history_frame = Frame(self, borderwidth=10)
+        self.history_frame.grid(row=0, column=0, columnspan=2)
 
         #############################
         # Center: Game board frame. #
         #############################
-        self.board_frame = tk.Frame(self, borderwidth=0)
-        self.board_frame.pack(side=tk.LEFT)
+        self.board_frame = Frame(self, borderwidth=10)
+        self.board_frame.grid(row=0, column=1, rowspan=8, columnspan=2)
+
+        self.minion_button = Button(self.board_frame, text='Minion 1\nMinion 2')
+        self.minion_button.pack()
 
         ######################
         # Right: Deck frame. #
         ######################
-        self.deck_frame = tk.Frame(self, borderwidth=0)
-        self.deck_frame.pack(side=tk.RIGHT)
+        self.deck_number = [IntVar(self, 0), IntVar(self, 0)]
 
-        self.deck_labels = [
-            tk.Label(self.deck_frame,
-                     text='Hello'),
-            tk.Label(self.deck_frame,
-                     text='World'),
-        ]
+        # Labels of deck numbers.
+        self.deck_label_0 = Label(
+            self,
+            borderwidth=10,
+            textvariable=self.deck_number[0]
+        )
+        self.deck_label_0.grid(row=0, column=9)
 
-        self.deck_labels[0].pack(
-            side=tk.TOP,
-            fill=tk.BOTH,
-            expand=True,
+        self.deck_label_1 = Label(
+            self,
+            borderwidth=10,
+            textvariable=self.deck_number[1]
         )
-        self.deck_labels[1].pack(
-            side=tk.BOTTOM,
-            fill=tk.BOTH,
-            expand=True,
-        )
+        self.deck_label_1.grid(row=1, column=9)
+
+        ############################
+        # Some initial operations. #
+        ############################
+
+        self.refresh_window()
+
+    def refresh_window(self):
+        """Refresh all elements of the game window."""
+
+        self.refresh_minions()
+        self.refresh_deck()
+
+    def refresh_minions(self):
+        for element in self.board_frame.winfo_children():
+            element.destroy()
+
+        for i in (0, 1):
+            player = self.game.players[i]
+
+            for n, card in enumerate(player.desk):
+                minion_button = Button(self.board_frame, text='Minion\n{}'.format(i))
+                minion_button.grid(row=2 if i == 0 else 1, column=n)
+
+            for n, card in enumerate(player.hand):
+                minion_button = Button(self.board_frame, text='Minion\n{}'.format(i))
+                minion_button.grid(row=3 if i == 0 else 0, column=n)
+
+    def refresh_deck(self):
+        for i in (0, 1):
+            self.deck_number[i].set(self.game.players[i].deck_number)
+
+    def _add_select(self):
+        self.selection_state += 1
+
+    def _clear_select(self):
+        self.selection_state = 0
+
+    # Some user operations.
+    def game_begin(self):
+        self.game.dispatch_event_quick(GameBegin)
+        self.refresh_window()
 
 
 __all__ = [
