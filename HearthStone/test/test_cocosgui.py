4c0a407913175b8c34820e99a99d6e77d5933711
fyabc <fyabc@mail.ustc.edu.cn>
2017-05-06 17:35:06 +0800

    Fix test_cocosgui.py.

--- HearthStone/test/test_cocosgui.py
+++ HearthStone/test/test_cocosgui.py
@@ -1,6 +1,11 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 from HearthStone.gui_tools.cocosgui.main import run_game
 
 __author__ = 'fyabc'
