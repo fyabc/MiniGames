8bfc280cd94c1c319c2a6b874711f4ef69725b0a
fyabc <fyabc@mail.ustc.edu.cn>
2018-05-10 19:26:52 +0800

    Change the version number.

--- HearthStone2/setup.py
+++ HearthStone2/setup.py
@@ -7,7 +7,7 @@ __author__ = 'fyabc'
 
 setup(
     name='MyHearthStone',
-    version='1.0.9',
+    version='1.1.0',
     keywords=('HearthStone', 'game'),
     description='A Python implementation of HearthStone.',
     license='MIT',
